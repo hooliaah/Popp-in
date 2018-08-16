b113dcde56b6c8da085e0d4c1ceef060a33b1a31
Julie Groth <groth.julie@gmail.com>
2018-08-16 16:24:54 -0700

    allow match with capital letters

--- assets/javascript/controller.js
+++ assets/javascript/controller.js
@@ -4,7 +4,7 @@ var loc;
 $("#submit").on("click", function (e) {
     e.preventDefault();
     var city = $("#search").val().trim();
-    if ((city).match(/[a-z]/)) {
+    if ((city).match(/[a-zA-Z]/)) {
         // $.getJSON is a method to get JSON data using an AJAX HTTP GET request
         // google geocode api is used for converting addresses
         // encodeURIComponent means it takes out any special characters
